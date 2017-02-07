45de2856b39e85c01c12544544557715712a4072
Andrey Andreyev <andrey.andreyev@corvidtec.com>
2017-02-07 18:08:22 -0500

    more work for home

--- 01_HW1/02_reachability/reachability.cpp
+++ 01_HW1/02_reachability/reachability.cpp
@@ -5,27 +5,47 @@ using std::vector;
 using std::pair;
 
 struct vertex{
-    vertex (): id(-1),isVisited(false),con(-1){}
+    vertex (): id(-1),isVisited(false),con(-1),nAdj(0){}
     int id;
     bool isVisited;
-    int con; 
+    int con;
+    int nAdj;
 };
 class graph{
     private:
         vector<vector<vertex> > adj;
         int n_vert;
+        vertex& getVert(int id){
+            return adj[id-1][0];
+        }
     public:
         void readGraph(){
             size_t n,m;
             std::cin>>n>>m;
             adj.resize(n,vector<vertex>());
+            this->n_vert=n;
             for (int i=0;i<m;i++){
                 int x,y;
                 std::cin>>x>>y;
                 adj[x-1].push_back(vertex());
                 adj[x-1].back().id=y;
+                adj[x-1].back().nAdj++;
                 adj[y-1].push_back(vertex());
                 adj[y-1].back().id=x;
+                adj[y-1].back().nAdj++;
+            }
+        }
+
+        void explore(int id){
+            vertex cur=this->getVert(id);
+            cur.isVisited=true;
+            int m=cur.nAdj;
+
+            for (int i=1;i<=m;i++){
+                vertex nxtVert=adj[id-1][i];
+                if (not nxtVert.isVisited){
+                    explore(nxtVert.id);
+                }
             }
         }
 };
@@ -37,6 +57,7 @@ int reach(vector<vector<int> > &adj, int x, int y) {
 int main() {
     graph test;
     test.readGraph();
+    test.explore(3);
   /*size_t n, m;
   std::cin >> n >> m;
   vector<vector<int> > adj(n, vector<int>());
